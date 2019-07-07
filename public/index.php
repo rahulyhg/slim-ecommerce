6cc61654551cef2c136166c3e128036fffd8e090
Philipp Nopper <PhilippN@eod.local>
2019-07-07 14:39:50 +0200

    Remove vendor folder

--- public/index.php
+++ public/index.php
@@ -2,13 +2,25 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+// Add DIC
+$container = new Container();
+
+// Add smarty view object
+$container->set('view', function() {
+    return new Smarty();
+}); 
+
+// Implement DIC
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write('hi.html');
     return $response;
 });
 
